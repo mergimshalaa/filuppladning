da0c8a34b876b2a42e880fc409e7c491f9b46a3c
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-17 16:41:11 +0200

    implement for every endpoint (WIP)

---
+++ src/file-controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+
+export async function getFileById(req: Request, res: Response) {
+// todo: implement
+}
+export async function uploadFile(req: Request, res: Response) {
+// todo: implement
+}
+
+export async function deleteFileById(req: Request, res: Response) {
+// todo: implement
+}
