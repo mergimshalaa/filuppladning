8836152fbb7ad6c7270868337400e94ebdd38af8
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-05-17 16:40:46 +0200

    endpoints to get, post, delete

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
 import express from "express";
+import { getFileById, uploadFile } from "./file-controller";
 
 export const app = express();
+
+app.get('/api/file/:id', getFileById);
+app.post('/api/file', uploadFile);
+app.delete('/api/file/:id', getFileById);
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import { app } from './app';
 
 async function main() {
     await mongoose.connect('mongodb://localhost:27017/upload-file');
-    console.log('Connected to database');
+    console.log("Connected to database");
     
     app.listen(3000, () => console.log('Server started at port 3000'));
 }
