c91a5e0faab47f42ee58fe1066993134b08fa662
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-06-07 00:11:21 +0200

    endpoint to get a file by id using fileBucket

--- src/file-controller.ts
+++ src/file-controller.ts
@@ -1,8 +1,20 @@
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { fileBucket } from './file-model';
 
 export async function getFileById(req: Request, res: Response) {
-// todo: implement
+    const _id = new mongoose.Types.ObjectId(req.params.id);
+
+    const file = await fileBucket.find({ _id });
+    if (!file) {
+        return res.status(404).json({ error: 'File not found' });
+    }
+
+    res.set('Content-Type', file.contentType);
+    const downloadStream = fileBucket.openDownloadStream(_id);
+  downloadStream.pipe(res);
 }
+
 export async function uploadFile(req: Request, res: Response) {
 // todo: implement
 }
